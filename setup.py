37b6e485bd2ded02a39d5574c7be1691e641cc3b
Steven Loria <sloria1@gmail.com>
2020-10-18 23:08:13 -0400

    Test against Python 3.8 and 3.9

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ INSTALL_REQUIRES = [
 
 EXTRAS_REQUIRE = {
     "tests": ["pytest"],
-    "lint": ["flake8==3.7.9", "flake8-bugbear==19.8.0", "pre-commit==1.21.0"],
+    "lint": ["flake8==3.7.9", "flake8-bugbear==19.8.0", "pre-commit~=2.7"],
 }
 EXTRAS_REQUIRE["dev"] = EXTRAS_REQUIRE["tests"] + EXTRAS_REQUIRE["lint"] + ["tox"]
 
@@ -61,6 +61,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Software Development :: Testing",
     ],
 )
